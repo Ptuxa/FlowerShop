f286a45b2f8e8482be419f17735d218e39582acb
Ptuxa <sdnaft@gmail.com>
2024-10-01 15:05:46 +0300

    fix: change index.tsx

--- FlowersShop.Spa/react-client/src/index.tsx
+++ FlowersShop.Spa/react-client/src/index.tsx
@@ -4,16 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Создаем корневой элемент с использованием createRoot API (React 18+)
+const rootElement = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Проверяем, существует ли rootElement
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement as HTMLElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+// Если вы хотите начать измерять производительность приложения, используйте reportWebVitals
 reportWebVitals();
